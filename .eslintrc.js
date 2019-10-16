726104ac4a6b9610ef42b152e2690c8fb73bb57a
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-16 12:01:46 -0300

    Inicial commit

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  rules: { 
+  //Faz algumas exceções às regras do eslint e adiciona outros módulos de erro
+    "prettier/prettier": "error",
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+};
