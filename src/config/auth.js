9f0bf30d43e2f25bcbcbdc7206f595f8d1169ef1
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-16 13:26:09 -0300

    chore: Configurated Database and Migrations

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'd6898430deceaf69cd54ba6ec78f8188',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const routes = new Router();
+
+routes.post('/sessions');
+
+export default routes;
